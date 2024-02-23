8bae5e924c1ade9e18dd0a15ad8e55a75a508d93
n.luparev <nikita.luparev@gmail.com>
2024-02-23 18:24:47 +0500

    active link

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,18 @@
-import { Outlet, Link, useLoaderData, Form } from 'react-router-dom'
+import {
+  Outlet,
+  Link,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+} from 'react-router-dom'
 
 import { getContacts, createContact } from '../contacts'
 
 export async function action() {
   const contact = await createContact()
-  return { contact }
+
+  return redirect(`/contacts/${contact.id}/edit`)
 }
 
 export async function loader() {
@@ -40,7 +48,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -49,7 +62,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{' '}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
